fac10205db9f2cd0e65b5e542ee02b85f3adcf94
Wcm <945469282@qq.com>
2018-04-13 10:27:23 +0000

    添加字体

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href='http://fonts.font.im/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/css/materialize.min.css">
     <link rel="stylesheet" href="/css/app.css">
     @yield('styles')
