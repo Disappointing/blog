6e73a5bcc9b0cde863b3e2e05dd7ef0a3135e9b7
Disappointing <945469282@qq.com>
2018-04-13 07:40:14 +0000

    优化访问速度

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="/css/materialize.min.css">
     <link rel="stylesheet" href="/css/app.css">
     @yield('styles')
