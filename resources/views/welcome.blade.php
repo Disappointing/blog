29aef861e64dce0f26507ec32cb56d63578e5f74
Disappointing <945469282@qq.com>
2018-04-13 07:17:32 +0000

    优化访问速度

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href='http://fonts.font.im/css?family=Ubuntu:700' rel='stylesheet' type='text/css'>
+   
 
     <title>DIE呆呆</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
 
     <!-- Styles -->
     <style>
