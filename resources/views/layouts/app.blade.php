5740cbfa948beb54309eeebfda60de720778b654
Disappointing <945469282@qq.com>
2018-04-14 08:50:58 +0000

    修改字体

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href='https://fonts.font.im/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/css/materialize.min.css">
     <link rel="stylesheet" href="/css/app.css">
     @yield('styles')
