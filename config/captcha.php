5c620162832da58c91a7b1e52b6b352d8ca35ee1
Disappointing <945469282@qq.com>
2018-05-15 01:40:07 +0000

    降低验证码难度

--- config/captcha.php
+++ config/captcha.php
@@ -12,11 +12,11 @@ return [
     ],
 
     'flat'   => [
-        'length'    => 6,
+        'length'    => 4,
         'width'     => 160,
         'height'    => 46,
         'quality'   => 90,
-        'lines'     => 6,
+        'lines'     => 4,
         'bgImage'   => false,
         'bgColor'   => '#ecf2f4',
         'fontColors'=> ['#2c3e50', '#c0392b', '#16a085', '#c0392b', '#8e44ad', '#303f9f', '#f57c00', '#795548'],
